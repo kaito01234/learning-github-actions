4881f5776a9f9c78643a65ec3b1ac87be2528162
kaito01234 <43839796+kaito01234@users.noreply.github>
2023-05-20 04:43:54 +0900

    add pages

--- codeceptjs/etc_test.ts
+++ codeceptjs/etc_test.ts
@@ -2,7 +2,7 @@ Feature("etc");
 
 Scenario("test something", ({ I }) => {
   I.amOnPage("http://localhost:3000");
-  I.seeInTitle('Create Next App');
+  I.seeInTitle('Create Next App3');
   I.wait(3);
   I.fillField('content',new Date());
   I.click('Save');
